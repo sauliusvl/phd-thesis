414f4594fc7725ac62226b2015b1189d83211ced
Saulius Valatka <saulius.vl@gmail.com>
2014-08-09 16:44:41 +0300

    short strings done

--- text/make-full.bat
+++ text/make-full.bat
@@ -1,4 +1,4 @@
-latex --src %1/main.tex --quiet --output-directory=%1
-pdflatex %1/main.tex --quiet --output-directory=%1
-rm %1/../thesis.pdf
-cp %1/main.pdf %1/../thesis.pdf
+latex --src %cd%/main.tex --quiet --output-directory="%cd%"
+pdflatex %cd%/main.tex --quiet --output-directory="%cd%"
+rm %cd%/../thesis.pdf
+cp %cd%/main.pdf %cd%/../thesis.pdf
